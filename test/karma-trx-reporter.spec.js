15049e6af8ec9abc5d862e64029e13727425e939
Michael Lehmann <lehmamic@bluewin.ch>
2014-10-17 18:30:57 +0200

    Start writing tests for the trx reporter

--- test/karma-trx-reporter.spec.js
+++ test/karma-trx-reporter.spec.js
@@ -1,7 +1,35 @@
 var assert = require("assert");
-describe('test skeleton', function(){
-    it('true does not equal fals', function(){
+var TrxReporter = require("../index")['reporter:trx'][1];
+
+describe('some noise', function(){
+    it('true does not equal false', function(){
 
         assert.equal(false, !true);
-    })
-})
+    });
+    it('Can resolve user', function(){
+        var user = process.env['USER'];
+        assert.equal(user!== undefined, true);
+    });
+});
+
+describe('karma-trx-reporter', function(){
+    it('can create instance', function(){
+        // act
+        var reporter = createReporter();
+
+        // assert
+        assert.equal(reporter !== undefined, true);
+    });
+
+});
+
+function createReporter() {
+    var config = { outputFile: './TestTrxResultsFile.trx' };
+    var emitter = { };
+    var logger = { create: function(logger) {} };
+    var helper = { };
+    var formatError = { };
+
+    var reporter = new TrxReporter(function(reporter){}, config, emitter, logger, helper, formatError);
+    return reporter;
+};
