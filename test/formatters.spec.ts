e7631ed9e43725e31fee78782410ae1bc2c0c1fe
Michael Lehmann <lehmamic@bluewin.ch>
2016-10-03 22:33:47 +0200

    write tests for the content of the trx file

---
+++ test/formatters.spec.ts
@@ -0,0 +1,11 @@
+import * as Formatters from '../src/Reporter/Formatters';
+
+describe('formatters', () => {
+    it('', () => {
+        // arrange
+
+        // act
+
+        // assert
+    });
+});
--- test/karma-trx-reporter.spec.ts
+++ test/karma-trx-reporter.spec.ts
@@ -41,24 +41,111 @@ describe('karma-trx-reporter', () => {
         expect(fakeFs.writeFile).to.have.been.called;
     });
 
-    // it('resulting trx file contains succeeded spec', () => {
-    //     // arrange
-    //     const xsdString: string = fs.readFileSync('./test/vstst.xsd', 'utf8');
-    //     const xsdDoc: XMLDocument = parseXmlString(xsdString);
+    it('resulting trx file single spec contains correct counters', () => {
+        // arrange
+        const fakeBrowser: karma.Browser = createFakeBrowser('Android', false, 1, 0, 0);
+        const fakeResult: karma.TestResult = createFakeResult('should not fail', true, false);
 
-    //     const fakeBrowser: karma.Browser = createFakeBrowser('Android', false, 1, 0, 0);
-    //     const fakeResult: karma.TestResult = createFakeResult('should not fail', true, false);
+        // act
+        simulateKarmaTestRun([ fakeBrowser ], [ fakeResult ]);
 
-    //     // act
-    //     simulateKarmaTestRun([ fakeBrowser ], [ fakeResult ]);
+        // assert
+        const  writtenXml = fakeFs.writeFile.firstCall.args[1];
+        expect(writtenXml).to.contain('<Counters total=\"1\" executed=\"1\" passed=\"1\" error=\"0\" failed=\"0\" timeout=\"0\" aborted=\"0\" inconclusive=\"0\" passedButRunAborted=\"0\" notRunnable=\"0\" notExecuted=\"0\" disconnected=\"0\" warning=\"0\" completed=\"0\" inProgress=\"0\" pending=\"0\"/>');
+    });
 
-    //     // assert
-    //     const  writtenXml = fakeFs.writeFile.firstCall.args[1];
-    //     const trxDoc = parseXmlString(writtenXml);
+    it('resulting trx file with multiple specs contains correct counters', () => {
+        // arrange
+        const fakeBrowser: karma.Browser = createFakeBrowser('Android', false, 1, 0, 0);
+        const fakeResultSuccess: karma.TestResult = createFakeResult('should not fail', true, false);
+        const fakeResultFailed: karma.TestResult = createFakeResult('should  fail', false, false);
+        const fakeResultSkipped: karma.TestResult = createFakeResult('should be skipped', false, true);
 
-    //     const isValidTrx: boolean = trxDoc.validate(xsdDoc);
-    //     expect(isValidTrx).to.be.true;
-    // });
+        // act
+        simulateKarmaTestRun([ fakeBrowser ], [ fakeResultSuccess, fakeResultFailed, fakeResultSkipped ]);
+
+        // assert
+        const  writtenXml = fakeFs.writeFile.firstCall.args[1];
+        expect(writtenXml).to.contain('<Counters total=\"3\" executed=\"2\" passed=\"1\" error=\"0\" failed=\"1\" timeout=\"0\" aborted=\"0\" inconclusive=\"0\" passedButRunAborted=\"0\" notRunnable=\"0\" notExecuted=\"1\" disconnected=\"0\" warning=\"0\" completed=\"0\" inProgress=\"0\" pending=\"0\"/>');
+    });
+
+    it('resulting trx file contains spec', () => {
+        // arrange
+        const fakeBrowser: karma.Browser = createFakeBrowser('Android', false, 1, 0, 0);
+        const fakeResult: karma.TestResult = createFakeResult('should not fail', true, false);
+
+        // act
+        simulateKarmaTestRun([ fakeBrowser ], [ fakeResult ]);
+
+        // assert
+        const  writtenXml = fakeFs.writeFile.firstCall.args[1];
+        expect(writtenXml).to.contain('name=\"Android_should not fail\"');
+    });
+
+    it('resulting trx file contains spec code base', () => {
+        // arrange
+        const fakeBrowser: karma.Browser = createFakeBrowser('Android', false, 1, 0, 0);
+        const fakeResult: karma.TestResult = createFakeResult('should not fail', true, false);
+
+        // act
+        simulateKarmaTestRun([ fakeBrowser ], [ fakeResult ]);
+
+        // assert
+        const  writtenXml = fakeFs.writeFile.firstCall.args[1];
+        expect(writtenXml).to.contain('codeBase=\"Sender.using it.get request\'.\'Android_should not fail\"');
+    });
+
+    it('resulting trx file contains spec class name', () => {
+        // arrange
+        const fakeBrowser: karma.Browser = createFakeBrowser('Android', false, 1, 0, 0);
+        const fakeResult: karma.TestResult = createFakeResult('should not fail', true, false);
+
+        // act
+        simulateKarmaTestRun([ fakeBrowser ], [ fakeResult ]);
+
+        // assert
+        const  writtenXml = fakeFs.writeFile.firstCall.args[1];
+        expect(writtenXml).to.contain('className=\"Sender.using it.get request\"');
+    });
+
+    it('resulting trx file contains succeeded spec outcome', () => {
+        // arrange
+        const fakeBrowser: karma.Browser = createFakeBrowser('Android', false, 1, 0, 0);
+        const fakeResult: karma.TestResult = createFakeResult('should not fail', true, false);
+
+        // act
+        simulateKarmaTestRun([ fakeBrowser ], [ fakeResult ]);
+
+        // assert
+        const  writtenXml = fakeFs.writeFile.firstCall.args[1];
+        expect(writtenXml).to.contain('outcome=\"Passed\"');
+    });
+
+    it('resulting trx file contains failed spec outcome', () => {
+        // arrange
+        const fakeBrowser: karma.Browser = createFakeBrowser('Android', false, 1, 0, 0);
+        const fakeResult: karma.TestResult = createFakeResult('should fail', false, false);
+
+        // act
+        simulateKarmaTestRun([ fakeBrowser ], [ fakeResult ]);
+
+        // assert
+        const  writtenXml = fakeFs.writeFile.firstCall.args[1];
+        expect(writtenXml).to.contain('outcome=\"Failed\"');
+    });
+
+    it('resulting trx file contains skipped spec outcome', () => {
+        // arrange
+        const fakeBrowser: karma.Browser = createFakeBrowser('Android', false, 1, 0, 0);
+        const fakeResult: karma.TestResult = createFakeResult('should fail', false, true);
+
+        // act
+        simulateKarmaTestRun([ fakeBrowser ], [ fakeResult ]);
+
+        // assert
+        const  writtenXml = fakeFs.writeFile.firstCall.args[1];
+        expect(writtenXml).to.contain('outcome=\"NotExecuted\"');
+    });
 
     function createFakeBrowser(name: string, error: boolean, success: number, skipped: number, failed: number): karma.Browser {
         return {
